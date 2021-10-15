5cb9ce32c51ec51c6f803a479c2fc8e68c731a6b
sandervanvugt <mail@sandervanvugt.nl>
2021-10-15 08:41:16 +0200

    updates

---
+++ setup_istio_vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# run this after starting minikube for Istio to work properly
+
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+export TCP_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="tcp")].nodePort}')
+export INGRESS_HOST=$(minikube ip)
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
